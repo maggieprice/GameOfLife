a06b2fcbb989697c14700ce2cd4536c755a48f40
Justin Price <mr.price@Justins-iMac.local>
2020-05-29 11:44:32 -0400

    Updates

---
+++ precache-manifest.cb1aaaec12138d2faea72d90435496ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a363f940bdaae85608f1397fd470dd7",
+    "url": "/GameOfLife/index.html"
+  },
+  {
+    "revision": "f6c4505ad44175e3aff2",
+    "url": "/GameOfLife/static/css/main.1e17faf8.chunk.css"
+  },
+  {
+    "revision": "404d07a8096027f8ec95",
+    "url": "/GameOfLife/static/js/2.3c141d07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GameOfLife/static/js/2.3c141d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c4505ad44175e3aff2",
+    "url": "/GameOfLife/static/js/main.d0774d97.chunk.js"
+  },
+  {
+    "revision": "1223e1a6c76a7dc083f8",
+    "url": "/GameOfLife/static/js/runtime-main.e49425fd.js"
+  }
+]);
